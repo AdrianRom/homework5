e3fb0d7089ff40e29d2ca332af1ad38669daeada
Adrian Romero <romeroaadrian@gmail.com>
2019-12-19 21:33:41 -0500

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 $(document).ready(function(){
+    ///////////////////////
+    var time_slots = $(".time-block");
+    var today = moment();
+    var right_now = today.hours();
+
+    for(var i = 0; i < time_slots.length; i++){
+        var description = $(time_slots[i]).find(".description");
+        var hour = getHour($(time_slots[i]).find(".hour").text());
+        if(localStorage[time_slots[i].id] != undefined){
+            console.log(time_slots[i], description, localStorage[time_slots[i].id], hour);
+            description.text(localStorage[time_slots[i].id]);
+        }
+        if(right_now == hour) description.addClass("present");
+        else if(hour < right_now) description.addClass("past");
+        else description.addClass("future");
+
+
+
+    }
+ 
+    function getHour(hourString) {
+
+        if(hourString.includes("AM")){
+            return parseInt(hourString.replace("AM",""));
+        }else{
+            var h = parseInt(hourString.replace("PM",""));
+
+            if(h < 12) return h + 12;
+            else return h;
+        }
+        
+    }
+
     $(".saveBtn").on("click", function() {
       console.log(this);
       var value = $(this).siblings(".description").val();
@@ -7,5 +40,8 @@ $(document).ready(function(){
       
       localStorage.setItem(time, value);
     });
-    $("#currentDay").text(moment());
+    $("#currentDay").text(moment().format('dddd, MMM Do YY'));
+    ///////////////////////
 });
+//to get the value back?
+// localStorage.setItem(value);
